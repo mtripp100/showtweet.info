89d80c3931f7de85b246d161f26371017eb3b4ca
Magnus Tripp <mtripp100@gmail.com>
2017-08-12 15:18:55 +0100

    Dump request back to user.

--- showtweet/views.py
+++ showtweet/views.py
@@ -1,6 +1,10 @@
-from flask import render_template
+from flask import render_template, request
 from showtweet import app
 
 @app.route('/')
 def hello():
     return render_template('base.html')
+
+@app.route('/', methods=['POST'])
+def id_posted():
+    return request.form['tweet_id']
