2d84804880d4dc2b5bd841e690443f5709ee76ca
Magnus Tripp <mtripp100@gmail.com>
2017-08-13 13:33:10 +0100

    pep8.

--- showtweet/__init__.py
+++ showtweet/__init__.py
@@ -4,14 +4,17 @@ from showtweet import tweet
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return render_template('base.html')
+    return render_template("base.html")
 
-@app.route('/', methods=['POST'])
+@app.route("/", methods=["POST"])
 def id_posted():
     try:
-        int(request.form['tweet_id'])
+        int(request.form["tweet_id"])
     except ValueError:
         return redirect(url_for("hello"))
-    return render_template('base.html', tweet_text=json.dumps(tweet.get_json(**request.form), indent=2, sort_keys=True))
+    return render_template("base.html",
+                           tweet_text=json.dumps(tweet.get_json(**request.form),
+                                                 indent=2,
+                                                 sort_keys=True))
--- showtweet/tweet.py
+++ showtweet/tweet.py
@@ -5,18 +5,21 @@ import requests
 import socket
 
 def obtain_bearer_token():
-    rq = requests.post("https://api.twitter.com/oauth2/token", headers={'Authorization': "Basic {}".format(_encode_keys()),
-                                                                        'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'},
-                       data='grant_type=client_credentials')
+    rq = requests.post("https://api.twitter.com/oauth2/token",
+                       headers={"Authorization": "Basic {}".format(_encode_keys()),
+                                "Content-Type": "application/x-www-form-urlencoded;charset=UTF-8"},
+                       data="grant_type=client_credentials")
+
     response = rq.json()
-    assert 'bearer' == response['token_type']
-    return response['access_token']
+    assert "bearer" == response["token_type"]
+    return response["access_token"]
 
 def _encode_keys():
-    consumer = urllib.parse.quote_plus(os.environ['CONSUMER_KEY'])
-    secret = urllib.parse.quote_plus(os.environ['CONSUMER_SECRET'])
+    consumer = urllib.parse.quote_plus(os.environ["CONSUMER_KEY"])
+    secret = urllib.parse.quote_plus(os.environ["CONSUMER_SECRET"])
     concatenated = "{}:{}".format(consumer, secret).encode()
     return base64.b64encode(concatenated).decode()
+
 
 TOKEN = obtain_bearer_token()
 
@@ -27,16 +30,17 @@ def get_json(tweet_id, **kwargs):
     rq = requests.get("https://api.twitter.com/1.1/statuses/show.json",
                       headers={"Authorization": "Bearer {}".format(TOKEN)},
                       params=params)
+
     record_metrics(rq.headers)
     return rq.json()
 
 def record_metrics(headers):
     metrics = {
-        "rate_limit_remaining": headers['x-rate-limit-remaining'],
-        "twitter_response_time": headers['x-response-time']
+        "rate_limit_remaining": headers["x-rate-limit-remaining"],
+        "twitter_response_time": headers["x-response-time"]
     }
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     for name, value in metrics.items():
-        metric = "{}.{} {}\n".format(os.environ['HOSTEDGRAPHITE_APIKEY'], name, value)
+        metric = "{}.{} {}\n".format(os.environ["HOSTEDGRAPHITE_APIKEY"], name, value)
         sock.sendto(metric.encode(), ("efc43c1b.carbon.hostedgraphite.com", 2003))
